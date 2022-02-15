216070d420077efd652fc334efc1c5cd051e0f5b
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-16 02:12:11 +0530

    contact reworked

--- components/contact/contact.component.tsx
+++ components/contact/contact.component.tsx
@@ -1,6 +1,6 @@
 export default function ContactComponent() {
   return (
-    <section className="bg-primary" style={{ height: "70vh" }}>
+    <section className="bg-primary" style={{ height: "75vh" }}>
       <div className="flex flex-col items-center pt-10">
         <div className="flex items-center justify-center text-5xl text-secondary">
           Contact
@@ -10,7 +10,7 @@ export default function ContactComponent() {
           style={{ width: "6rem", height: "0.3rem" }}
         ></hr>
 
-        <form className="mt-10">
+        <form className="mt-20">
           <input
             className="outline-none px-2 py-5 bg-primaryDark text-secondary"
             type={"text"}
@@ -37,7 +37,7 @@ export default function ContactComponent() {
           <div className="flex justify-end">
             <input
               type={"submit"}
-              className="mt-2 outline outline-2 outline-secondary text-secondary px-4 py-2 hover:bg-accent hover:outline-accent transition-all duration-500"
+              className="mt-5 outline outline-2 outline-secondary text-secondary px-4 py-2 hover:bg-accent hover:outline-accent transition-all duration-500"
             />
           </div>
         </form>
