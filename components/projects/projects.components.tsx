09cdd1c6a0334e7355652d8a393fca3ba1a7c966
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-16 00:34:16 +0530

    particle bug fixed

---
+++ components/projects/projects.components.tsx
@@ -0,0 +1,8 @@
+export default function ProjectComponent() {
+  return (
+    <div className="">
+      <div className=""> Hello world</div>
+      <div className="mb-20"></div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import ParticlesBg from "../components/home/particle";
 import Image from "next/image";
+import HomeComponent from "../components/home/home.component";
+import ProjectComponent from "../components/projects/projects.components";
 
 const Home: NextPage = () => {
   return (
@@ -13,26 +15,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div className="flex flex-col justify-center items-center w-screen h-screen">
-          <div className="flex flex-row">
-            <div className="mr-2 text-5xl text-secondary">Hello, I'm</div>
-            <div className="text-primary text-5xl">Raveen</div>
-          </div>
-          <div className="text-white text-5xl mt-3">
-            I'm a full-stack developer
-          </div>
-          <div className="mb-20"></div>
-          <a
-            className="group outline outline-2 outline-secondary px-4 py-2 hover:bg-primary hover:outline-primary transition-all duration-500"
-            href="#"
-          >
-            <div className="flex justify-center items-center">
-              <div className="text-2xl text-secondary">View works</div>
-              <div className="text-2xl text-secondary ml-4 group-hover:rotate-90 transition-all duration-500">-></div>
-            </div>
-          </a>
-        </div>
-        <ParticlesBg></ParticlesBg>
+        <HomeComponent />
+        <ProjectComponent />
       </main>
     </div>
   );
