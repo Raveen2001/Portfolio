38a261a62c3d7a139e735a0da28407ea2242c0e6
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-15 22:46:11 +0530

    particle js component added

---
+++ components/home/particle.tsx
@@ -0,0 +1,76 @@
+import Particles from "react-tsparticles";
+
+export default function ParticlesBg() {
+  return (
+    <>
+      <Particles
+        className="relative -z-10"
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#252934",
+            },
+          },
+          fpsLimit: 360,
+          interactivity: {
+            events: {
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              repulse: {
+                distance: 100,
+                duration: 0.1,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.1,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.4,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 4,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Particles from "react-tsparticles";
-import styles from "../styles/Home.module.css";
-
+import ParticlesBg from "../components/home/particle";
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="m-0 p-0">
       <Head>
         <title>Raveen</title>
         <meta name="description" content="Portfolio of Raveen" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="m-0 p-0">
-        <Particles
-          id="tsparticles"
-          options={{
-            background: {
-              color: {
-                value: "#252934",
-              },
-            },
-            fpsLimit: 360,
-            interactivity: {
-              events: {
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                resize: true,
-              },
-              modes: {
-                repulse: {
-                  distance: 100,
-                  duration: 0.1,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.1,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outMode: "bounce",
-                random: false,
-                speed: 1,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 50,
-              },
-              opacity: {
-                value: 0.4,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                random: true,
-                value: 4,
-              },
-            },
-            detectRetina: true,
-          }}
-        />
+      <main>
+        <h2 className="text-white">Hello world</h2>
+        <ParticlesBg></ParticlesBg>
       </main>
     </div>
   );
