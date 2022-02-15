60f6709f2af4f9dedb3ff6755ab0cd87c43068fa
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-16 01:19:54 +0530

    project screen partially completed

--- components/projects/projects.components.tsx
+++ components/projects/projects.components.tsx
@@ -1,13 +1,30 @@
 export default function ProjectComponent() {
   return (
-    <div className="h-screen bg-primary flex flex-col items-center">
-      <div className="flex items-center justify-center text-5xl text-secondary">
-        Projects
+    <section className="bg-primary" style={{ height: "105vh" }}>
+      <div className="flex flex-col items-center pt-10">
+        <div className="flex items-center justify-center text-5xl text-secondary">
+          Projects
+        </div>
+        <hr
+          className="bg-secondary mt-4"
+          style={{ width: "6rem", height: "0.3rem" }}
+        ></hr>
+
+        <div
+          className="grid grid-cols-3 grid-rows-3 gap-0 h-screen mt-10"
+          style={{ height: "80vh", width: "80vw" }}
+        >
+          <div className="bg-secondary">1</div>
+          <div className="bg-accent">2</div>
+          <div className="bg-secondary">3</div>
+          <div className="bg-accent">4</div>
+          <div className="bg-secondary">5</div>
+          <div className="bg-accent">6</div>
+          <div className="bg-secondary">7</div>
+          <div className="bg-accent">8</div>
+          <div className="bg-secondary">9</div>
+        </div>
       </div>
-      <hr
-        className="bg-secondary"
-        style={{ width: "6rem", height: "0.3rem" }}
-      ></hr>
-    </div>
+    </section>
   );
 }
