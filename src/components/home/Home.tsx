00787e3b898b3a324b374777b061643b4bf4c25c
Raveen A <raveen.a@maersk.com>
2023-01-03 18:47:09 +0530

    particles added

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { ReactComponent as LinkedInIcon } from "../../assets/linkedin.svg";
 import { ReactComponent as GithubIcon } from "../../assets/github.svg";
+import { ReactComponent as MouseIcon } from "../../assets/arrow_right.svg";
 
 import "./Home.scss";
+import Button from "../button/Button";
+import Particles from "../particles/Particles";
 
 const Home = () => {
   return (
     <div className="Home">
+      <Particles />
       <div className="main-content">
-        <h1>Raveen</h1>
-        <i>Frontend developer</i>
-        <button>Hello</button>
+        <div className="left">
+          <h1 className="name">Raveen</h1>
+          <h3 className="designation">
+            <i>Frontend developer</i>
+          </h3>
+          <Button text="About Me" onClick={() => {}} />
+        </div>
+        <div className="right">
+          <a href="#">
+            <LinkedInIcon />
+          </a>
+          <a href="#">
+            <GithubIcon />
+          </a>
+        </div>
       </div>
-      <div className="side-content">
-        <a href="#">
-          <LinkedInIcon />
-        </a>
-        <a href="#">
-          <GithubIcon />
-        </a>
-      </div>
+      <a className="bottom-content" href="#">
+        <span>Works</span>
+        <MouseIcon width={50} height={46} />
+      </a>
     </div>
   );
 };
