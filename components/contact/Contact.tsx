e06f5b324a5bc09841f970850bb59e1948756002
Raveen2001 <raveenanbarasan@gmail.com>
2023-05-25 19:55:53 +0530

    updated contact info

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -96,17 +96,14 @@ const Contact = () => {
             <h3>About Me.</h3>
             <p>Interactive Front-end developer</p>
             <div>
-              I'm Raveen, a <strong>Fullstack Web and Mobile developer</strong>.
-              I'm a <strong>cool guy </strong>
-              who likes to build ideas that pop out with web and mobile
-              technologies. I have <strong>2 years experience</strong> in web
-              and mobile development. I like to
-              <strong>
-                {" "}
-                create new stuffs, resolve logical problems, solving critical
-                bugs
-              </strong>
-              .Actually for hire.
+              I am Raveen, a{" "}
+              <strong>talented Fullstack Web and Mobile developer</strong> with
+              a passion for building innovative ideas using cutting-edge web and
+              mobile technologies. With <strong>2 years of experience</strong>,
+              I excel in creating unique solutions, solving complex problems,
+              and addressing critical bugs.
+              <strong>I am actively seeking for a Full Time Opportunity</strong>
+              .
             </div>
 
             <Splide
