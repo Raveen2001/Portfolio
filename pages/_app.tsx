344feff5c1e960f1fed561be27f27b678bd555c2
raveen <raveenanbarasan@gmail.com>
2024-01-29 14:06:43 +0530

    Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
