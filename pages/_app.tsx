e43ebc67efecf4308e48bb76abfadd691a6a8480
raveen <raveenanbarasan@gmail.com>
2024-01-29 14:11:50 +0530

    speed insights added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics />
     </>
   );
